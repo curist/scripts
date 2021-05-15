d568187bf5c0574f6d53811e94532896d09b2022
curist <curist.cyc@gmail.com>
2021-05-15 21:32:23 +0800

    stylish thing

--- swap-workspace.clj
+++ swap-workspace.clj
@@ -15,16 +15,13 @@
       (process '[jq -r ".[] | select(.focused==true).name"])
       :out slurp trim Integer/parseInt))
 
-(defn swap-it [direction]
-  (let [[MIN_WS MAX_WS] [1 9]
-        ws1 (get-current-workspace)
-        ws2 (if (= direction "left") (dec ws1) (inc ws1))
-        oob (or (= ws1 ws2)
-                (> ws1 MAX_WS) (> ws2 MAX_WS)
-                (< ws1 MIN_WS) (< ws2 MIN_WS))]
-    (when-not oob (move-workspace ws1 ws2))))
-
-(let [[arg-direction] *command-line-args*
-      direction (or arg-direction "left")]
-  (swap-it direction))
+(let [[MIN_WS MAX_WS] [1 9]
+      direction (nth *command-line-args* 0 "left")
+      ws1 (get-current-workspace)
+      ws2 (if (= direction "left") (dec ws1) (inc ws1))
+      oob (or (= ws1 ws2)
+              (> ws1 MAX_WS) (> ws2 MAX_WS)
+              (< ws1 MIN_WS) (< ws2 MIN_WS))]
+  (when-not oob
+    (move-workspace ws1 ws2)))
 
--- swap-workspace.fnl
+++ swap-workspace.fnl
@@ -4,8 +4,6 @@
 (local i3msg (sh.command :i3-msg))
 (local jq (sh.command :jq))
 
-(local [MIN_WS MAX_WS] [1 9])
-
 (fn move-workspace [ws1 ws2]
   (i3msg (.. "'"
              "rename workspace " ws1 " to temporary; "
@@ -17,12 +15,13 @@
       (jq "-r '.[] | select(.focused==true).name'")
       tostring tonumber))
 
-(let [direction (or (. arg 1) :left)
+(let [[MIN_WS MAX_WS] [1 9]
+      direction (or (. arg 1) :left)
       ws1 (get-current-workspace)
-      ws2 (if (= direction :left) (- ws1 1) (+ ws1 1))]
-  (when (or (= ws1 ws2)
-            (> ws1 MAX_WS) (> ws2 MAX_WS)
-            (< ws1 MIN_WS) (< ws2 MIN_WS))
-    (lua :return))
-  (move-workspace ws1 ws2))
+      ws2 (if (= direction :left) (- ws1 1) (+ ws1 1))
+      oob (or (= ws1 ws2)
+              (> ws1 MAX_WS) (> ws2 MAX_WS)
+              (< ws1 MIN_WS) (< ws2 MIN_WS))]
+  (when (not oob)
+    (move-workspace ws1 ws2)))
 
